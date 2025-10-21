8c59b7c8d567e1a5821e23459d8aa4bd46f54176
MAXTIT73 <mkosanovsky@gmail.com>
2025-10-21 16:26:25 +0200

    Add files via upload

---
+++ MK/ff.py
@@ -0,0 +1,14 @@
+while(True):
+    print("| Welcome |")
+    print("-------------------")
+    print("| wybierz opcje |")
+    print("1) opcja 1")
+    print("2) opcja 2")
+    print("3) opcja 3")
+    print("4) opcja 4")
+    print("q) wyjście")
+    ff = input("| opcja nr: ")
+    if ff == 'q':
+        break
+    if ff == 1:
+        print(4**6+45/4)
